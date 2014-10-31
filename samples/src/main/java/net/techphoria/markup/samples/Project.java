6f5c8ce1a8fa8481f27202c339fce625782529b3
Paul Wardrip <pwardrip@gmail.com>
2014-10-31 18:34:33 -0400

    Working on sample code.

---
+++ samples/src/main/java/net/techphoria/markup/samples/Project.java
@@ -0,0 +1,38 @@
+package net.techphoria.markup.samples;
+
+public class Project {
+	private String first;
+	private String last;
+	private String project;
+	private String version;
+	
+	public String getFirst() {
+		return first;
+	}
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	
+	public String getLast() {
+		return last;
+	}
+	public void setLast(String last) {
+		this.last = last;
+	}
+	
+	public String getProject() {
+		return project;
+	}
+	public void setProject(String project) {
+		this.project = project;
+	}
+	
+	public String getVersion() {
+		return version;
+	}
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	
+	
+}
---
+++ samples/src/main/webapp/jsp/sample.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<%@ taglib prefix="mm" uri="http://techphoria.net/markup-machine" %>
+<%@ include file="include.jspf" %>
+<head>
+	<title>Markup Machine Sample #1</title>
+</head>
+<body>
+	<h1>Three Different Ways to Express Data in JSP</h1>
+
+	<h2>Properties of an Object:</h2>
+	<mm:template url="../template.html">
+		
+	</mm:template>
+</body>
+</html>
